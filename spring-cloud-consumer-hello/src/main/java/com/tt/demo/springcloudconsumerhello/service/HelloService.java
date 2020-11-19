8224271f7eeb7adc64f28d0fac7cd169e0dc9a63
janjun <janjunt@163.com>
2020-11-19 09:23:05 +0800

    断路器由Hystrix变更为sentinel

--- spring-cloud-consumer-hello/src/main/java/com/tt/demo/springcloudconsumerhello/service/HelloService.java
+++ spring-cloud-consumer-hello/src/main/java/com/tt/demo/springcloudconsumerhello/service/HelloService.java
@@ -1,11 +1,11 @@
 package com.tt.demo.springcloudconsumerhello.service;
 
-import com.tt.demo.springcloudconsumerhello.service.hystrix.HelloServiceHystrix;
+import com.tt.demo.springcloudconsumerhello.service.fallback.HelloFallbackService;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "spring-cloud-producer-hello", fallback = HelloServiceHystrix.class)
+@FeignClient(name = "spring-cloud-producer-hello", fallback = HelloFallbackService.class)
 public interface HelloService {
     @GetMapping("/hello/{name}")
     String hello(@PathVariable("name") String name);
--- spring-cloud-consumer-hello/src/main/java/com/tt/demo/springcloudconsumerhello/service/fallback/HelloFallbackService.java
+++ spring-cloud-consumer-hello/src/main/java/com/tt/demo/springcloudconsumerhello/service/fallback/HelloFallbackService.java
@@ -1,10 +1,10 @@
-package com.tt.demo.springcloudconsumerhello.service.hystrix;
+package com.tt.demo.springcloudconsumerhello.service.fallback;
 
 import com.tt.demo.springcloudconsumerhello.service.HelloService;
 import org.springframework.stereotype.Component;
 
 @Component
-public class HelloServiceHystrix implements HelloService {
+public class HelloFallbackService implements HelloService {
     @Override
     public String hello(String name) {
         return "hello " + name + ", this messge send failed";

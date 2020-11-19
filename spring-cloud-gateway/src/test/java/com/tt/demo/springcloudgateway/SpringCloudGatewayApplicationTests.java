95467b1b538cf4e91da546ade242b7987b253c81
janjun <janjunt@163.com>
2020-11-19 09:56:28 +0800

    集成网关spring-cloud-gateway

---
+++ spring-cloud-gateway/src/test/java/com/tt/demo/springcloudgateway/SpringCloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tt.demo.springcloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

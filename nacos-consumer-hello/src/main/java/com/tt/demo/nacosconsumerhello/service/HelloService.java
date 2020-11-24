e27e1a6a3cdd14d5a65dfa4367747098fc61e18b
janjun <janjunt@163.com>
2020-11-24 17:53:44 +0800

    module重命名

--- nacos-consumer-hello/src/main/java/com/tt/demo/nacosconsumerhello/service/HelloService.java
+++ nacos-consumer-hello/src/main/java/com/tt/demo/nacosconsumerhello/service/HelloService.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "spring-cloud-producer-hello", fallback = HelloFallbackService.class)
+@FeignClient(name = "nacos-producer-hello", fallback = HelloFallbackService.class)
 public interface HelloService {
     @GetMapping("/hello/{name}")
     String hello(@PathVariable("name") String name);

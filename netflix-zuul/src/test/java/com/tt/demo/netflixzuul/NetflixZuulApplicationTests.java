411bdc6893dab44cd6dcebe7e98aaadb58d47feb
janjun <janjunt@163.com>
2020-11-25 10:42:59 +0800

    集成zuul网关

---
+++ netflix-zuul/src/test/java/com/tt/demo/netflixzuul/NetflixZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tt.demo.netflixzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
